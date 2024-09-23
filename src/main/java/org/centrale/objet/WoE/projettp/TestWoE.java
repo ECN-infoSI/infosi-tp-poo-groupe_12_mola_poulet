addcf8f23bdca013647ca9227c86c612a663f694
clesp <corentin.poulet@eleves.ec-nantes.fr>
2024-09-23 16:06:47 +0200

    Nouveaux tests

--- src/main/java/org/centrale/objet/WoE/projettp/TestWoE.java
+++ src/main/java/org/centrale/objet/WoE/projettp/TestWoE.java
@@ -18,22 +18,18 @@ public class TestWoE {
         //Initialisation du monde
         Archer a=new Archer("Robin des bois",50,10,75,50,50,5,new Point2D(0,0),10);
         Paysan p=new Paysan("Malon",50,10,75,50,50,5,new Point2D(0,0));
-        Lapin l1=new Lapin(50,10,75,50,50,new Point2D(0,0));
-        Lapin l2=new Lapin(50,10,75,50,50,new Point2D(0,0));
+        Lapin l1=new Lapin(50,10,10,50,50,new Point2D(0,0));
+        Lapin l2=new Lapin(50,10,10,50,50,new Point2D(0,0));
         Loup lo=new Loup(50,10,75,50,50,new Point2D(0,0));
         Epee arme=new Epee("Master Sword",30,new Point2D(0,0));
-        Guerrier g=new Guerrier ("Link",50,10,75,50,50,5,arme,new Point2D(0,0));
+        Guerrier g=new Guerrier ("Link",50,10,10,50,50,5,arme,new Point2D(0,0));
         PotionSoin s=new PotionSoin(10,new Point2D(0,0));
         World w=new World(a,p,l1,l2,lo,g,s);
         //génération du monde
         w.creeMondeAlea();
-        w.getRobin().affiche();
-        w.getGuillaumeT().affiche();
-        w.getPeon().affiche();
-        w.getRoger().affiche();
-        w.getBugs().affiche();
+        w.afficheWorld();
         //déplacements
-        w.getBugs().deplace();
+        /*w.getBugs().deplace();
         w.getBugs().affiche();
         w.getPeon().deplace(2, -3);
         w.getPeon().affiche();
@@ -43,6 +39,17 @@ public class TestWoE {
         w.getPeon().deplace(2);
         w.getPeon().affiche();
         w.getBugs().deplace(6);
+        w.getBugs().affiche();*/
+        //Tests de combat et de soin
+        w.getGrosBill().setPos(new Point2D (w.getBugs().getPos().getX()+1,w.getBugs().getPos().getY()));
+        w.getGrosBill().combattre(w.getBugs());
         w.getBugs().affiche();
+        w.getRobin().setPos(new Point2D (w.getGrosBill().getPos().getX()+3,w.getGrosBill().getPos().getY()));
+        w.getRobin().combattre(w.getGrosBill());
+        w.getRobin().affiche();
+        w.getGrosBill().affiche();
+        w.getGrosBill().setPos(new Point2D (w.getPotion().getPos().getX(),w.getPotion().getPos().getY()));
+        w.getGrosBill().soin(w.getPotion());
+        w.getGrosBill().affiche();
     }
 }
