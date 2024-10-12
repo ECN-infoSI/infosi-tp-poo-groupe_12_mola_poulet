256f8e3519a571bd9a6f1fa852677b2d2466eefe
clesp <corentin.poulet@eleves.ec-nantes.fr>
2024-10-12 14:41:29 +0200

    Test des nouvelles fonctionnalités

--- src/main/java/org/centrale/objet/WoE/projettp/TestWoE.java
+++ src/main/java/org/centrale/objet/WoE/projettp/TestWoE.java
@@ -3,7 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package org.centrale.objet.WoE.projettp;
-import java.util.ArrayList;
 import java.util.Random;
 /**
  *
@@ -18,32 +17,29 @@ public class TestWoE {
     public static void main(String[] args){
         //Initialisation du monde
         World w=new World();
+        w.getJoueur().demande();
         Random r= new Random();
         w.creeMondeAlea(r.nextInt(21), r.nextInt(21), r.nextInt(21), r.nextInt(21), r.nextInt(21), r.nextInt(21), r.nextInt(21));
         w.afficheWorld();
         
-        World w2=new World(1000,1000);
-        w2.creeMondeAlea(20, 20, 20, 20, 20, 0, 0);
+        //Tests des performances de la matrice
+        World w2=new World(10000,10000,new Joueur());
+        w2.getJoueur().demande();
+        w2.creeMondeAlea(1000, 1000, 1000, 10000, 100000, 1000, 1537);
+        w2.afficheWorld();
         int sum=0;
         long debut=System.nanoTime();
-        for (Entite p:w2.getListeEntite()){
-            if (p instanceof Creature){
-                sum+=((Creature) p).getPtVie();
-            }
+        int j=w2.getListeEntite().length;
+        for (int i=0;i<j;i++){
+           for (Entite p:w2.getListeEntite()[i]){
+                if (p instanceof Creature){
+                    sum+=((Creature) p).getPtVie();
+                }
+           }
+            
         }
         System.out.println("Points de vie totaux : "+sum);
         long fin=System.nanoTime();
-        System.out.println("Temps écoulé en ns : "+(fin-debut));
-        sum=0;
-        debut=System.nanoTime();
-        ArrayList<Entite> list=w2.getListeEntite();
-        for(int i=0;i<list.size();i++){
-            if (list.get(i) instanceof Creature){
-                sum+=((Creature) w2.getListeEntite().get(i)).getPtVie();
-            }
-        }
-        fin=System.nanoTime();
-        System.out.println("Temps écoulé en ns : "+(fin-debut));
-        
+        System.out.println("Temps écoulé en ns : "+(fin-debut)); 
     }
 }
