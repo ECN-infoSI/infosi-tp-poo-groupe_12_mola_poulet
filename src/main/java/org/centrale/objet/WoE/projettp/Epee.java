3499a709e11ef9d81cf333941a2b14c2a5998772
Amolz <Amolz@130.66.205.68>
2024-10-14 17:58:21 +0200

    implementation interface Utilisable

--- src/main/java/org/centrale/objet/WoE/projettp/Epee.java
+++ src/main/java/org/centrale/objet/WoE/projettp/Epee.java
@@ -4,11 +4,13 @@
  */
 package org.centrale.objet.WoE.projettp;
 
+import static java.sql.JDBCType.NULL;
+
 /**
  *
  * @author clesp
  */
-public class Epee extends Objet {
+public class Epee extends Objet implements Utilisable{
     private String name;
     private int bonus;
 
@@ -83,6 +85,33 @@ public class Epee extends Objet {
     @Override
     public void affiche(){
         System.out.println("[Nom de l'arme : "+this.name+", Bonus de l'arme : "+this.bonus+"]");
+    }
+    
+    @Override
+    public void utilisation(Joueur player){
+        
+        if (!(player.getPerso() instanceof Guerrier)){
+            System.out.println("seul un guerrier peut equiper une epee");
+        }
+        else{
+            Guerrier rambo = ((Guerrier)player.getPerso());
+            
+            if (rambo.getArme() == null){
+                
+                rambo.setArme(this);
+                rambo.setDegAtt(rambo.getDegAtt() + bonus);
+                rambo.getInventaire().add(this)
+                
+            }
+            else{
+                rambo.setDegAtt(rambo.getDegAtt()-rambo.getArme().getBonus() + this.bonus);
+                rambo.setArme(this);
+                rambo.getInventaire().remove(rambo.getArme());
+                rambo.getInventaire().add(this);
+                
+            }
+        }
+        
     }
     
 }
