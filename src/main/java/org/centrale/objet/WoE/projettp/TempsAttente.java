6d5c0d4830ba1fd62711685fabf0e3f6e051a28a
Amolz <Amolz@130.66.205.68>
2024-10-14 16:46:24 +0200

    creation intefaces TempsAttente et Utilisable

---
+++ src/main/java/org/centrale/objet/WoE/projettp/TempsAttente.java
@@ -0,0 +1,16 @@
+package org.centrale.objet.WoE.projettp;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author Amolz
+ */
+public interface TempsAttente {
+    
+    public void expiration();
+    
+}
---
+++ src/main/java/org/centrale/objet/WoE/projettp/Utilisable.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package org.centrale.objet.WoE.projettp;
+
+/**
+ *
+ * @author Amolz
+ */
+public interface Utilisable {
+    
+    /**
+     * permet au joueur d'utiliser un consommable
+     * @param player
+     */
+    public void utilisation(Joueur player);
+    
+}
