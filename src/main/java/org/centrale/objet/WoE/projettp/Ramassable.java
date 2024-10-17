0195d95ceac7b294ee7c35b383729c56bfaa2276
Amolz <Amolz@130.66.205.68>
2024-10-17 18:46:09 +0200

    creation interface

--- src/main/java/org/centrale/objet/WoE/projettp/Ramassable.java
+++ src/main/java/org/centrale/objet/WoE/projettp/Ramassable.java
@@ -1,9 +1,9 @@
 package org.centrale.objet.WoE.projettp;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
+
 
 /**
  *
